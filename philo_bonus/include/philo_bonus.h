7412681ce0a4d3cf5aad25409d8404253d7b15cc
Mou-SED <moussa.seddik.ph@gmail.com>
2022-06-01 19:01:36 +0100

    chore: Init bonus part

---
+++ philo_bonus/include/philo_bonus.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/01 18:49:56 by moseddik          #+#    #+#             */
+/*   Updated: 2022/06/01 18:54:57 by moseddik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_BONUS_H
+# define PHILO_BONUS_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <sys/wait.h>
+# include <semaphore.h>
+
+#endif
