29f03f461396466830962440006f4358b770d829
moseddik <moussa.seddik.ph@gmail.com>
2022-06-08 16:26:31 +0100

    feat: create child process

--- philo_bonus/src/create_process.c
+++ philo_bonus/src/create_process.c
@@ -6,34 +6,44 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 13:14:06 by moseddik          #+#    #+#             */
-/*   Updated: 2022/06/07 15:31:46 by moseddik         ###   ########.fr       */
+/*   Updated: 2022/06/08 16:19:21 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
 
-void	create_process(void)
+int run_proccess(t_philos *philosophers)
 {
-	// pid_t 	c_pid;
-	int 	is_create;
-	int 	i;
+	pthread_t	t1;
+	philosophers = NULL;
+	printf("Child - %d\n", getpid());
+	if (pthread_create(&t1, NULL, &routine, &philosophers))
+		printf("Error\n");
+	pthread_join(t1, NULL);
+	exit(0);
+}
 
-	i = 0;
-	while (i < 4)
+void	create_process(t_philos *philosophers)
+{
+	int			*is_create;
+	int			i;
+
+	is_create = malloc(sizeof(int) * philosophers->num_philos);
+	i = -1;
+	while (++i < philosophers->num_philos)
 	{
-		is_create = fork();
-		i++;
-		if (is_create == -1)
+		is_create[i] = fork();
+		if (is_create[i] == -1)
 			exit(1);
-		if (is_create != 0)
+		if(is_create[i] == 0)
 		{
-			waitpid(is_create, NULL, 0);
-			printf("Parent - %d\n", getpid());
-		}
-		else
-		{
-			printf("Child - %d\n", getpid());
-			exit(0);
+			run_proccess(philosophers);
 		}
 	}
+	i = -1;
+	while (++i < 4)
+		waitpid(-1, NULL, 0);
+	i = -1;
+	while (++i < 4)
+		printf("\nis_create value : %d\n", is_create[i]);
 }
