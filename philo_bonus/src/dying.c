c14136901b7ee1247aee71d2c4e68a36164dbd37
moseddik <moussa.seddik.ph@gmail.com>
2022-06-10 20:58:24 +0100

    feat: Add dying conditions

---
+++ philo_bonus/src/dying.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dying.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/10 17:16:23 by moseddik          #+#    #+#             */
+/*   Updated: 2022/06/10 20:55:23 by moseddik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_bonus.h"
+
+static int	is_die(t_philos *philosophers)
+{
+	philosophers->philo_data->philo_die
+		= set_time(philosophers->philo_data->init_time);
+	if (philosophers->philo_data->philo_die - philosophers->last_eat
+		>= philosophers->philo_data->time_to_die)
+		return (1);
+	return (0);
+}
+
+// TODO:free childs
+
+void	dying(t_philos *philosophers, pid_t *list_pid)
+{
+	(void)list_pid;
+	while (1)
+	{
+		if (is_die(philosophers))
+		{
+			print_routine(NULL,
+				set_time(philosophers->philo_data->init_time), philosophers);
+			exit(2);
+		}
+		if (philosophers->philo_data->philos_eats == 1)
+		{
+			exit(3);
+		}
+	}
+}
