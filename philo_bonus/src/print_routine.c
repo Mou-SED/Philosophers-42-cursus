f7772f79d61976eebf9037c3e4f1e4d040aeaf98
moseddik <moussa.seddik.ph@gmail.com>
2022-06-10 21:01:16 +0100

    refactor: Add semaphore for print

--- philo_bonus/src/print_routine.c
+++ philo_bonus/src/print_routine.c
@@ -6,7 +6,7 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 16:21:10 by moseddik          #+#    #+#             */
-/*   Updated: 2022/06/08 16:22:10 by moseddik         ###   ########.fr       */
+/*   Updated: 2022/06/10 20:53:12 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 void	print_routine(char *message, time_t time, t_philos *philosophers)
 {
+	sem_wait(philosophers->philo_data->print_sem);
+	if (message == NULL)
+	{
+		printf("%ld %d %s\n", time, philosophers->id + 1, "died");
+		return ;
+	}
 	printf("%ld %d %s\n", time, philosophers->id + 1, message);
+	sem_post(philosophers->philo_data->print_sem);
 }
